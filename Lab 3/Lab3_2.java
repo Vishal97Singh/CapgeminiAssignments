5c46eb0fcd8a56321c427b6b53a5111cf375b99d
Vishal97Singh <42646954+Vishal97Singh@users.noreply.github.com>
2019-03-19 23:55:26 +0530

    Add files via upload

--- Lab 3/Lab3_2.java
+++ Lab 3/Lab3_2.java
@@ -2,13 +2,14 @@ package lab2;
 
 import java.awt.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Lab3_2 {
 	
 	public static String[] getSorted(String[] s) {
 		int size=s.length;
 		String []s1=new String[size];
-	
+	    Arrays.sort(s);
 		for(int i=0;i<size;i++) {
 			if(i<(size+1)/2) {
 				s1[i]=s[i].toUpperCase();		
